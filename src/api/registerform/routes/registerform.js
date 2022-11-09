9a4b26ff04023b207edd4568865fa5b835f2132b
NJ-Nitank <nitank.jadaun@ens.enterprises>
2022-11-09 18:43:10 +0530

    Register form api created

---
+++ src/api/registerform/routes/registerform.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * registerform router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::registerform.registerform');
---
+++ src/api/registerform/services/registerform.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * registerform service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::registerform.registerform');
